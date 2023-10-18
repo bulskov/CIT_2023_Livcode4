07426ad19a2d38322559df3a27442b1cb5e53736
bulskov <bulskov@ruc.dk>
2023-10-18 11:36:10 +0200

    Extend service

--- DataLayer/DataService.cs
+++ DataLayer/DataService.cs
@@ -17,4 +17,35 @@ public class DataService
         return db.Categories.FirstOrDefault(x => x.Id == categoryId);
         //return db.Categories.Find(categoryId);
     }
+
+    public bool DeleteCategory(Category category)
+    {
+        return DeleteCategory(category.Id);
+    }
+    public bool DeleteCategory(int categoryId)
+    {
+        var db = new NorthwindContex();
+        var category = db.Categories.FirstOrDefault(x => x.Id == categoryId);
+        if(category != null)
+        {
+            db.Categories.Remove(category);
+            //db.Remove(category);
+            return db.SaveChanges() > 0;
+        }
+        return false;
+    }
+
+    public Category CreateCategory(string name, string description) {
+        var db = new NorthwindContex();
+        var id = db.Categories.Max(x => x.Id) + 1;
+        var category = new Category
+        {
+            Id = id,
+            Name = name,
+            Description = description
+        };
+        db.Add(category);
+        db.SaveChanges();
+        return category;
+    }
 }
--- DbApp/Program.cs
+++ DbApp/Program.cs
@@ -4,6 +4,8 @@ using DataLayer;
 
 var ds = new DataService();
 
+ds.CreateCategory("some name", " asfsdsdfsdf");
+
 var categories = ds.GetCategories();
 
 foreach (var entity in categories)
