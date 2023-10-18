bb491e2bf75795741af4ea708fbdbb17b73dd92e
bulskov <bulskov@ruc.dk>
2023-10-18 08:52:18 +0200

    connect to db with ADO

--- ADO/Program.cs
+++ ADO/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Npgsql;
+
+var connectionString = "host=localhost;db=northwind;uid=bulskov;pwd=henrik";
+
+var connection = new NpgsqlConnection(connectionString);
+connection.Open();
+
+var cmd = new NpgsqlCommand();
+cmd.Connection = connection;
+cmd.CommandText = "select * from categories";
+
+var rdr = cmd.ExecuteReader();
+
+while(rdr.Read())
+{
+    Console.WriteLine($"{rdr.GetInt32(0)} {rdr.GetString(1)}");
+}
