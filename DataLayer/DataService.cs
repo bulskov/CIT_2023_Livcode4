b475a6fb9f9e9db7e6488901cdefa615a5b92a69
bulskov <bulskov@ruc.dk>
2023-10-18 11:28:30 +0200

    add datalayer abstraction

---
+++ DataLayer/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace DataLayer;
+
+public class DataService
+{
+    
+    public IList<Category> GetCategories()
+    {
+        var db = new NorthwindContex();
+        return db.Categories.ToList(); 
+    }
+
+    public Category? GetCategory(int categoryId)
+    {
+        var db = new NorthwindContex();
+        return db.Categories.FirstOrDefault(x => x.Id == categoryId);
+        //return db.Categories.Find(categoryId);
+    }
+}
--- DbApp/Program.cs
+++ DbApp/Program.cs
@@ -1,1 +1,12 @@
 ﻿
+
+using DataLayer;
+
+var ds = new DataService();
+
+var categories = ds.GetCategories();
+
+foreach (var entity in categories)
+{
+    Console.WriteLine(entity);
+}
