61f9a5cc610e336c9610a2cc4c275e5a4603bb05
bulskov <bulskov@ruc.dk>
2023-10-18 10:34:54 +0200

    First EF example

---
+++ EF/NorthwindContex.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF;
+
+public class NorthwindContex : DbContext
+{
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .LogTo(Console.Out.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+        optionsBuilder.UseNpgsql("host=localhost;db=northwind;uid=bulskov;pwd=henrik");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().ToTable("categories");
+        modelBuilder.Entity<Category>()
+            .Property(x => x.Id).HasColumnName("categoryid");
+        modelBuilder.Entity<Category>()
+            .Property(x => x.Name).HasColumnName("categoryname");
+        modelBuilder.Entity<Category>()
+            .Property(x => x.Description).HasColumnName("description");
+    }
+}
---
+++ EF/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using EF;
+
+var db = new NorthwindContex();
+
+foreach (var category in db.Categories.Where(x => x.Id > 5))
+{
+    Console.WriteLine(category);
+}
